b4fcf25f29d1a372dbfc17d1a194f6a63b175051
s21161 <boniekfacyx@gmail.com>
2021-07-03 13:45:52 +0200

    kolos

---
+++ jaz-43c_s21161-nbp/src/main/java/pl/pjatk/jaz43c_s21161nbp/Jaz43cS21161NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jaz43c_s21161nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaz43cS21161NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jaz43cS21161NbpApplication.class, args);
+	}
+
+}
---
+++ jaz-43c_s21161-nbp/src/test/java/pl/pjatk/jaz43c_s21161nbp/Jaz43cS21161NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jaz43c_s21161nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaz43cS21161NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
